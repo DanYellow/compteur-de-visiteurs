a971d0d2750844545c4bcac1ff355617ebadd935
Danielo Jean-Louis (DanYellow) <jeanlouis.danielo@sfr.fr>
2025-09-17 16:00:47 +0200

    Add form schema

--- src/assets/scripts/sign-in-form.ts
+++ src/assets/scripts/sign-in-form.ts
@@ -1,5 +1,25 @@
 import * as z from "zod";
 
+const MemberSchema = z.object({
+    prenom: z.string(
+    ).min(1, { message: "You must enter a password" }),
+    nom: z.string(),
+    numero_telephone: z.string(),
+    email: z.string(),
+    adresse: z.string(),
+    ville: z.string(),
+    // region: z.string(),
+    code_postal: z.string(),
+    habitant: z.string(),
+    association: z.string(),
+    faclab: z.string(),
+}).refine((data) => {
+    return false;
+}, {
+    error: "Choissez",
+    path: ["fefee"]
+})
+
 const form = document.querySelector("[data-sign-in-form]") as HTMLFormElement;
 
 const submitForm = (e: SubmitEvent) => {
@@ -7,7 +27,9 @@ const submitForm = (e: SubmitEvent) => {
 
     const formData = new FormData(e.target);
 
-    console.log(formData);
+    const validation = MemberSchema.safeParse(Object.fromEntries(formData));
+
+    console.log(validation);
 };
 
 form?.addEventListener("submit", submitForm);
