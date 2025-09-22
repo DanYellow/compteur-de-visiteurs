d207de6c3f82e99b10f742f09d018a70e2826ca4
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-09-22 14:15:33 +0200

    Ajout du système de notification lors de la réception du socket

--- src/assets/scripts/members-poll.ts
+++ src/assets/scripts/members-poll.ts
@@ -1,6 +1,24 @@
-const ws = new WebSocket('ws://localhost:3900');
+const ws = new WebSocket(`ws://localhost:${import.meta.env.VITE_PORT}`);
 
-const rowTplRaw = document.querySelector("tr[data-member-row]") as HTMLTemplateElement;
+const rowTplRaw = document.querySelector("tr[data-member-row]") as HTMLTableRowElement;
+const notificationTplRaw = document.querySelector("[data-template-id='new-member-notification']") as HTMLTemplateElement;
+const notificationsContainer = document.querySelector("[data-notifications-container]");
+const nbMembers = document.querySelector("[data-nb-members]");
+
+const createNotification = (member: { nom: string, prenom: string }) => {
+    const notification = notificationTplRaw.content.cloneNode(true) as HTMLDivElement;
+    (notification.querySelector("[data-notification-text]") as HTMLParagraphElement).textContent = `${member.prenom} ${member.nom.toUpperCase()} vient de souscrire à la charte du FacLab®`;
+    (notification.querySelector("[data-dismiss-notification-btn]") as HTMLButtonElement).addEventListener("click", (e) => {
+        const element = e.target as HTMLButtonElement;
+        element?.closest("[data-notification]")?.classList.add("close")
+        element?.closest("[data-notification]")?.addEventListener("transitionend", (evt) => {
+            const element = evt.target as HTMLDivElement;
+            element.parentNode?.removeChild(element);
+        })
+    });
+
+    notificationsContainer?.append(notification);
+}
 
 ws.addEventListener("message", (event) => {
     const { payload, type } = JSON.parse(event.data);
@@ -14,7 +32,11 @@ ws.addEventListener("message", (event) => {
             if (cell) {
                 cell.textContent = String(value);
             }
-        })
+        });
         tbody!.appendChild(rowTpl);
+
+        nbMembers!.textContent = String(Number(nbMembers!.textContent) + 1);
+
+        createNotification(payload);
     }
 });
