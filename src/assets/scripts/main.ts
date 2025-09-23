45e723ab1a349c11e3ef6a57f65c2b18a49a6d9d
Danielo Jean-Louis (DanYellow) <jeanlouis.danielo@sfr.fr>
2025-09-23 11:18:17 +0200

    Ajout fonctionnalité de fermeture de <dialog>

--- src/assets/scripts/main.ts
+++ src/assets/scripts/main.ts
@@ -1,1 +1,9 @@
 import "../styles/main.css";
+
+document.querySelectorAll("dialog").forEach((item) => {
+    item.addEventListener("click", (e) => {
+        if ("closeDialog" in (e.target! as HTMLElement).dataset) {
+            (e.currentTarget! as HTMLDialogElement).close();
+        }
+    })
+})
